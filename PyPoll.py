4f1423fc4026a7d63b6c91eaf0ee9fc2111e975b
Lisa Ann Yu <lisaann.yu@gmail.com>
2022-08-23 19:36:52 -0700

    read header

--- PyPoll.py
+++ PyPoll.py
@@ -5,22 +5,18 @@ import os
 # Assign a variable for the file to load and the path
 file_to_load = os.path.join('Resources', 'election_results.csv')
 
-# # Open the election results and read the file
-# with open(file_to_load) as election_data:
-
-#      # To do: perform analysis.
-#      print(election_data)
-
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+# Open the election results and read the file
+with open(file_to_load) as election_data:
 
-    # Write some data to the file.
-    # txt_file.write("Hello World")
-    # Write three counties to the file.
-    txt_file.write("Arapahoe, Denver, Jefferson")
+     # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
 
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
